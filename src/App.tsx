f5d344c358cc84a9a52e3adb907800dbd8c39d6c
Andrew Russell <arustle@pm.me>
2020-04-12 17:17:20 -0400

    feat: add start template

---
+++ src/App.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import {IonApp, IonIcon, IonLabel, IonRouterOutlet, IonTabBar, IonTabButton, IonTabs} from '@ionic/react';
+import { images, listOutline, homeOutline } from 'ionicons/icons';
+import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
+import Items from './pages/Items';
+import RecordView from './pages/RecordView.route';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+const App: React.FC = () => (
+  <IonApp>
+    <IonReactRouter>
+        <IonTabs>
+          <IonRouterOutlet>
+            <Route path="/home" component={Home} exact={true} />
+            <Route path="/items" component={Items} exact={true} />
+            <Route path="/records/:recordId" component={RecordView} exact={true} />
+            <Route exact path="/" render={() => <Redirect to="/home" />} />
+          </IonRouterOutlet>
+          <IonTabBar slot="bottom">
+              <IonTabButton tab="tab1" href="/home">
+                  <IonIcon icon={homeOutline} />
+                  <IonLabel>Home</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab2" href="/tab2">
+                  <IonIcon icon={images} />
+                  <IonLabel>Photos</IonLabel>
+              </IonTabButton>
+              <IonTabButton tab="tab3" href="/items">
+                  <IonIcon icon={listOutline} />
+                  <IonLabel>Items</IonLabel>
+              </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+    </IonReactRouter>
+  </IonApp>
+);
+
+export default App;
