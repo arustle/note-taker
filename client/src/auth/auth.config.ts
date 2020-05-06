81eb6cbc882b424d5770711eafe367aa728513b1
Andrew Russell <arustle@pm.me>
2020-05-05 20:29:09 -0400

    feat: add serverless

--- client/src/auth/auth.config.ts
+++ client/src/auth/auth.config.ts
@@ -1,7 +1,8 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'chlwem5wbf'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+// const apiId = 'rjxa11xk05'
+// export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 // export const apiEndpoint = `http://localhost:3000/dev`
+export const apiEndpoint = `https://api.measuringchange.com/dev`
 
 export const AUTH_CONFIG = {
   clientId: 'jaGPHsP26hXndaxXddaLR4jN2wmqYsZZ',
